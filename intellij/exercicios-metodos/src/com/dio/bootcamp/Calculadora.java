3bb435de6643e6fc9f0d7c5b0fe64a6c48913610
JuKieling <jgkg0805@gmail.com>
2022-07-28 19:24:12 -0300

    Exercícios de métodos: criação de uam calculadora.

---
+++ intellij/exercicios-metodos/src/com/dio/bootcamp/Calculadora.java
@@ -0,0 +1,24 @@
+package com.dio.bootcamp;
+
+public class Calculadora {
+
+    public static void soma(float a, float b){
+        float resultado = a + b;
+        System.out.println("O resultado  da soma é: " + resultado);
+    }
+    public static void subtracao(float a, float b){
+        float resultado = a - b;
+        System.out.println("O resultado  da subtração é: " + resultado);
+    }
+    public static void divisao(float a, float b){
+        float resultado = a / b;
+        System.out.println("O resultado  da divisão é: " + resultado);
+    }
+    public static void multiplicacao(float a, float b){
+        float resultado = a * b;
+        System.out.println("O resultado  da multiplicação é: " + resultado);
+    }
+
+
+}
+
---
+++ intellij/exercicios-metodos/src/com/dio/bootcamp/Main.java
@@ -0,0 +1,10 @@
+package com.dio.bootcamp;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora.soma(12, 5);
+        Calculadora.subtracao(12, 5);
+        Calculadora.divisao(12, 5);
+        Calculadora.multiplicacao(12, 5);
+        }
+}
